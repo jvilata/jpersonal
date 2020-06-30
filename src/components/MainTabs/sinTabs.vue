56c7b982d6efd791f8248b7c8904623b2a3e92fa
jvilata <jvilata@edicom.es>
2020-06-30 16:34:47 +0200

    Auto stash before rebase of "jvilata"

--- src/components/MainTabs/sinTabs.vue
+++ src/components/MainTabs/sinTabs.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="text-h6">Seleccione opción del menú</div>
+  <div>
+    <div class="text-h6">Seleccione opción del menú</div>
+    <q-btn  flat label="prueba" color="primary" @click="prueba"/>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
+    }
+  },
+  methods: {
+    prueba () {
+      this.$axios.get('pru.asp')
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
