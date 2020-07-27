60f34cca184a5a069bc7794625d4889cf4f6a8e7
jvilata <jvilata@edicom.es>
2020-07-27 17:35:22 +0200

    overflow hidden en permisos

--- src/components/Permisos/permisosMain.vue
+++ src/components/Permisos/permisosMain.vue
@@ -188,14 +188,17 @@ export default {
   
   .fullScreen {
     height: calc(100vh - 184px);
+    overflow: hidden;
   }
 
   .sqScreen {
     height: calc(100vh - 128px);
+    overflow: hidden;
   }
 
   .android {
     height: calc(100vh - 107px);
+    overflow: hidden;
   }
 
 </style>
