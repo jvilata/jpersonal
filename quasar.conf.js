6e5630efe95a019452c2fd6c7967d66bf176f76f
Javier <javierhcer@gmail.com>
2020-07-30 17:27:30 +0200

    v0.0.4

--- quasar.conf.js
+++ quasar.conf.js
@@ -154,7 +154,7 @@ cfg.module.rules.push({
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
       id: 'com.edicom.jpersonal',
-      version: '0.0.3'
+      version: '0.0.4'
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
