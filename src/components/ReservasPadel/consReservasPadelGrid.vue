b712061ada3d53aa1d7b75a52ae6dcb7592f2841
Marta <mmoya@edicomgruop.com>
2022-04-13 17:14:58 +0200

    Ampliamos el numero de días para gestionar las reservas de padel

--- src/components/ReservasPadel/consReservasPadelGrid.vue
+++ src/components/ReservasPadel/consReservasPadelGrid.vue
@@ -330,8 +330,8 @@ export default {
       const fsol = date.extractDate(this.filterRecord.fechaDesde + ':00','YYYY-MM-DDTHH:mm:ss')
       this.dialogSalaR1 = false
       // this.$refs.dialogSalaR1.hide()
-      if (![16, 33, 36, 37].includes(this.user.pers.area) && (fsol > date.addToDate(new Date(), { days: 7 }))) {
-        this.$q.dialog({ title: 'Aviso', message: 'No puede reservar pista a más de 7 días vista' })
+      if (![16, 33, 36, 37].includes(this.user.pers.area) && (fsol > date.addToDate(new Date(), { days: 14 }))) {
+        this.$q.dialog({ title: 'Aviso', message: 'No puede reservar pista a más de 14 días vista' })
         return
       }
       var solapa = false
